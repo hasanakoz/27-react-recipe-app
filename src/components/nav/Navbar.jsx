5700f1f911a25f90963943aa9ca2d9e1fc10ad9f
hasan <hasanakoz1986@gmail.com>
2022-10-31 22:59:01 +0300

    navbar styled components done

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.style";
+import { Brand, Nav, Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  return (
+    <Nav justify="space-between" wrap="wrap">
+      <Brand to="/">
+        <i>{"h1 />"}</i>
+        <span>Recipe</span>
+      </Brand>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="logout" onClick={() => sessionStorage.clear()}>
+          Logout
+        </MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
