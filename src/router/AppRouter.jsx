e3943db526956fb42876d1adb7c7176e2d4819ea
hasan <hasanakoz1986@gmail.com>
2022-10-31 21:02:11 +0300

    Browser Router, Routes, AppRouter done

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
+import Navbar from "../components/nav/Navbar";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
-    <div>
-      App Router
-    </div>
-  )
-}
+    <BrowserRouter>
+      <GlobalStyles />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="detail" element={<PrivateRouter />}>
+          <Route path="" element={<Detail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
