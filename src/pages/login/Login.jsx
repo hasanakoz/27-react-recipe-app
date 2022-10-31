a1e08f6dde6884ea7b8755e14aaeb7c11ba5ee6e
hasan <hasanakoz1986@gmail.com>
2022-10-31 21:40:29 +0300

    Login page styled components done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,40 @@
 import React from "react";
-import { LoginContainer } from "./Login.style";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import { useNavigate } from "react-router-dom";
+import meal from "../../assets/meal.svg";
 
 const Login = () => {
-  return <LoginContainer></LoginContainer>;
+  const navigate = useNavigate();
+
+  const userInfo = {
+    username: "admin",
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>RECIPE</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter username" type="text" />
+          <StyledInput placeholder="password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
